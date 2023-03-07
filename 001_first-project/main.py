078d03469c8afaf614313995d007ec8747426ec8
Reariuz <118771123+Reariuz@users.noreply.github.com>
2023-03-07 20:44:26 +0000

    SQL_Alchemy added

--- 001_first-project/main.py
+++ 001_first-project/main.py
@@ -1,7 +1,13 @@
 # save this as app.py
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def hello():
