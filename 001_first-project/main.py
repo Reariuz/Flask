82a040604e598dbc8566682cc1c14987b9faa313
Reariuz <118771123+Reariuz@users.noreply.github.com>
2023-03-07 22:51:59 +0000

    created db

--- 001_first-project/main.py
+++ 001_first-project/main.py
@@ -1,6 +1,7 @@
 # save this as app.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -8,10 +9,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user = db.Column(db.String(200), nullable = True)
+    content = db.Column(db.String(500), nullable = True)
+    created_at = db.Column(db.DateTime(), default= datetime.utcnow)
+
+
+
+@app.route("/create")
+def create():
+    db.create_all()
+    return "DB created"
+
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
